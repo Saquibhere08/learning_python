c2c38bc0e66cfacd951fda0cd0b50d2d2ed586a9
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-07-15 17:10:23 +0530

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,14 @@
+"""Dictionary is nothing but key value pairs"""
+
+d1={}
+print(type(d1))
+
+d2= {"Harry": "Burger",
+     "Rohan":"Fish",
+     "Saquib":"Roti",
+     "Shubham":{"B":"Maggie",
+                "L":"Roti",
+                "D":"Chicekn"}}
+
+print(d2["Shubham"])
+print(d2)
