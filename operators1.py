a7570487b516efbe223fb6810ddf4a85e055ca4a
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-08-02 18:30:19 +0530

    Add files via upload

---
+++ operators1.py
@@ -0,0 +1,38 @@
+#Arithmetic Operators
+print("5+5 is ", 5+5)
+print("5-5 is ", 5-5)
+print("5/5 is ", 5/5)
+print("5*5 is ", 5*5)
+print("5**5 is ", 5**5)
+print("15//6 is ", 15//6)
+print("5%5 is ", 5%5)
+
+#assignment Operators
+
+x=5
+print(x)
+
+#Comparison Operators
+i=8
+print(i!=5)
+print(i<=5)
+print(i>=5)
+
+#Logical Operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+
+#identity Operators
+print(a is not b)
+print(5 is not a)
+
+#Membership Operators
+list=[3,2,1,4,56,4,334,22,32]
+print(234 not in list)
+
+#Bitwise Operators
+print(0 & 1 )
+print(0 | 1)
+print(0 | 3)
