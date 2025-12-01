e77cc7a28904d3f1808d9090a78abe86ec9a6458
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-01 07:02:38 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+a=10
+b=20
+
+print("Value of a before swap: ",a)
+print("Value of b before swap: ",b)
+
+#Swap Operation
+(a,b)=(b,a) 
+
+#print the Swapped Values: 
+print("value of a after swap: ",a)
+print("Value of b after swap: ",b)
